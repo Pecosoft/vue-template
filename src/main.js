4662801511dfc74573238e2414d119d9ad02d0fc
bliky <blikylee@126.com>
2018-09-15 13:53:59 +0800

    [add feature]component: ProfileHeader

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Play from 'components/ProfileHeader'
+import 'style/pui.scss'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { Play },
-  template: '<Play/>'
+  template: '<div class="page"><div class="container"><Play/></div></div>'
 })
