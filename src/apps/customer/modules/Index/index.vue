7029989b4e32be63d2cb405bcc25426eea72f54f
bliky <blikylee@126.com>
2018-09-18 00:42:24 +0800

    [add]首页添加Grid组件调用

--- src/apps/customer/modules/Index/index.vue
+++ src/apps/customer/modules/Index/index.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 div
   profile-header(:name='name' :desc='desc' :avatar='avatar')
+  grid(:data='gridData' :col-num="2" bg-color="#fff" @clickgrid="handleOnClickGrid")
 </template>
 
 <script>
@@ -10,7 +11,18 @@ export default {
     return {
       name: '台群精机',
       desc: '台群阳光服务，在线报修',
-      avatar: 'static/logo.png'
+      avatar: 'static/logo.png',
+      gridData: [
+        {id: 1, name: '报修', icon: 'repaire'},
+        {id: 2, name: '咨询', icon: 'consult'},
+        {id: 3, name: '投诉', icon: 'complain'},
+        {id: 4, name: '建议', icon: 'suggest'}
+      ]
+    }
+  },
+  methods: {
+    handleOnClickGrid: gd => {
+      console.log('click on grid', gd.id)
     }
   }
 }
