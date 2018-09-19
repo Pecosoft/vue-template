46e95dd4b08ba383e706c80e86c68c852957c90b
bliky <blikylee@126.com>
2018-09-20 07:54:26 +0800

    OrderList组件基本布局

--- src/components/OrderList/item.vue
+++ src/components/OrderList/item.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
-  .peco-order-list-item
-    h3 {{ thisData.sn }}
-    p {{ thisData.desc }}
-    p {{ thisData.status }}
-    div
-      img(:src='thisData.product.avatar')
-      p {{ thisData.product.name }}
-    p {{ thisData.create_time }}
-    p
-      span(@click.stop='remove') 删除
+  section.peco-order-item
+    .peco-order-item_hd.aux 订单号:&nbsp;
+      span {{ thisData.sn }}
+      span.frt {{ thisData.status|status-text }}
+    .peco-order-item_bd
+      .peco-order-item_inner
+        img(:src='thisData.product.avatar')
+        .flex1
+          p {{ thisData.product.name }}
+          p {{ thisData.desc }}
+      p 所属区域:&nbsp;{{ thisData.addr||'地址' }}
+    .peco-order-item_ft
+      span {{ thisData.create_time|time-text }}
+      span.frt(@click.stop='remove') 删除
 </template>
 
 <script>
@@ -17,6 +21,24 @@ import listItem from '@/mixins/listItem'
 export default {
   name: 'ListItem',
   mixins: [listItem],
+  filters: {
+    statusText (val) {
+      return val === 0 ? '未受理'
+        : val === 1 ? '已受理'
+        : val === 2 ? '已派单'
+        : val === 3 ? '维修中'
+        : val === 4 ? '已完成'
+        : val === 5 ? '已评论' : 'unknown';
+    },
+    timeText (val) {
+      if (String(val).length !== 13) val *= 1000;
+
+      let dt = new Date(val);
+
+      return dt.getFullYear() + '-' + (dt.getMonth()+1) + '-' + dt.getDate() + ' '
+         + dt.getHours() + ':' + dt.getMinutes() + ':' + dt.getSeconds()
+    }
+  },
   mounted () {
     //console.log(this.thisData, this.list)
   }
@@ -32,9 +54,50 @@ export default {
 @import 'style/mixin/_clearfix.scss';
 @import 'style/extend/_layout.scss';
 
-.peco-order-list-item {
+.peco-order-item {
+  background: $color-pannel-bg;
+  padding: $edge-distance;
   &+& {
-    margin-top: 20px;
+    border-top: 1px solid $color-border;
   }
+}
+
+.peco-order-item_hd {
+  padding: 0 0 10px;
+  border-bottom: 1px solid $color-border2;
+}
+
+.peco-order-item_bd {
+  padding: 10px 0;
+  border-bottom: 1px solid $color-border2;
+}
+
+.peco-order-item_inner {
+  @extend %flex-row;
+  padding-bottom: 10px;
+  img {
+    @include size(80px);
+  }
+  .flex1 {
+    padding: 0 0 0 $edge-distance;
+    p+p {
+      margin-top: $edge-distance;
+    }
+  }
+}
+
+.peco-order-item_ft {
+  padding: 10px 0 0;
+}
+
+.flex1 {
+  flex: 1;
+}
+
+.aux {
+  font-size: $font-size-aux;
+}
+.frt {
+  float: right;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ new Vue({
   components: { Play },
   data: {
     listData: [
-      {id: 1, sn: '2018050210524290585581', status: 1, product: {name: 'T-500H', avatar: 'http://iph.href.lu/80x80?text=产品图片'}, desc: '故障', create_time: new Date().getTime()},
-      {id: 2, sn: '2017110810335533171752', status: 1, product: {name: 'T-500H', avatar: 'http://iph.href.lu/80x80?text=产品图片'}, desc: '故障', create_time: new Date().getTime()},
-      {id: 3, sn: '2017110810320688829954', status: 1, product: {name: 'T-500H', avatar: 'http://iph.href.lu/80x80?text=产品图片'}, desc: '故障', create_time: new Date().getTime()},
-      {id: 4, sn: '2017102621550936318893', status: 1, product: {name: 'T-500H', avatar: 'http://iph.href.lu/80x80?text=产品图片'}, desc: '故障', create_time: new Date().getTime()}
+      {id: 1, sn: '2018050210524290585581', addr: '广东省深圳市南山区', status: 0, product: {name: 'T-500H', avatar: 'http://iph.href.lu/80x80?text=产品图片'}, desc: '故障', create_time: new Date().getTime()},
+      {id: 2, sn: '2017110810335533171752', addr: '广东省深圳市南山区', status: 1, product: {name: 'T-500H', avatar: 'http://iph.href.lu/80x80?text=产品图片'}, desc: '故障', create_time: new Date().getTime()},
+      {id: 3, sn: '2017110810320688829954', addr: '广东省深圳市南山区', status: 2, product: {name: 'T-500H', avatar: 'http://iph.href.lu/80x80?text=产品图片'}, desc: '故障', create_time: new Date().getTime()},
+      {id: 4, sn: '2017102621550936318893', addr: '广东省深圳市南山区', status: 3, product: {name: 'T-500H', avatar: 'http://iph.href.lu/80x80?text=产品图片'}, desc: '故障', create_time: new Date().getTime()}
     ]
   },
   methods: {
@@ -20,5 +20,5 @@ new Vue({
       //this.$refs.list.remove(d)
     }
   },
-  template: '<div class="page"><div class="container"><Play ref=list @clicklist="handleOnClickList" :data="listData" /></div></div>'
+  template: '<div class="page"><div class="container"><Play style="margin: 20px 0;" ref=list @clicklist="handleOnClickList" :data="listData" /></div></div>'
 })
