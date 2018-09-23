6c2d2393ef1facbfa9521a94d868e3aa0498e7dd
bliky <blikylee@126.com>
2018-09-23 11:47:53 +0800

    [add]Repair Consult Complain Suggest.

---
+++ src/apps/customer/modules/Repair/create.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+page 我要报修
+</template>
+
+<script>
+import { repair } from '@/models'
+import create from '@/mixins/create'
+
+export default {
+  mixins: [create],
+  data () {
+    return {
+      model: repair
+    }
+  },
+  mounted() {
+    console.log(this.validator.toString())
+    console.log(this.form)
+    console.log('verify', this.verify())
+    this.submit().then(res => console.log(res))
+  },
+  methods: {
+    createApi (formData) {
+      return {
+        code: 200,
+        data: formData
+      }
+    }
+  }
+}
+</script>
