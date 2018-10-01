2dbdbb155312212c9e3d6e906745f0ee8dc90c8f
bliky <blikylee@126.com>
2018-10-01 18:43:27 +0800

    [add]apps/customer增加ModelInput调用

--- src/apps/customer/router/index.js
+++ src/apps/customer/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from './routes'
+import routesModelInput from 'components/ModelInput/router/routes'
 import { beforeEach, afterEach } from '@/hooks/router'
+import prefixRoutes from 'utils/prefixRoutes'
 
 Vue.use(Router)
 
 const router = new Router({
   mode: 'history',
-  routes
+  routes: routes.concat(prefixRoutes('/customer', routesModelInput))
 })
 
 router.beforeEach(beforeEach)
