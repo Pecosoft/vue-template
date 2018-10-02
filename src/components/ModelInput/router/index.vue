b6cf6b655fcefa1832ff4cc29c925ea3d79162ab
bliky <blikylee@126.com>
2018-10-02 13:42:03 +0800

    [add]新增组件Button

--- src/components/ModelInput/router/index.vue
+++ src/components/ModelInput/router/index.vue
@@ -1,6 +1,19 @@
 <template lang='pug'>
   page
-    model-input(:model='model')
+    pannel
+      model-input(:model='model')
+    btn-area
+      btn-primary(loading) 保存提交
+      btn 保存
+      btn-default 保存
+      btn-default(disable) 保存
+      btn-default(plain) 保存
+      btn-default(plain disable) 保存
+      btn-primary 保存
+      btn-primary(disable) 保存
+      btn-primary(plain) 保存
+      btn-primary(plain disable) 保存
+      btn-warn 保存
 </template>
 
 <script>
