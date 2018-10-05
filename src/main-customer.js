185edf82076a98dfab42e2cbc0813dd97109a70f
bliky <blikylee@126.com>
2018-10-05 09:41:13 +0800

    [add]增加Create组件调用

--- src/main-customer.js
+++ src/main-customer.js
@@ -16,6 +16,12 @@ import { ModelInput } from 'components/ModelInput'
 import Search from 'components/Search'
 import GroupList from 'components/GroupList'
 import Button from 'components/Button'
+import Create from 'components/Create'
+import Dialog from 'components/Dialog'
+
+// Global plugins
+import LoadingPlugin from 'plugins/loading'
+import ToastPlugin from 'plugins/toast'
 
 Vue.use(Page)
 Vue.use(ProfileHeader)
@@ -27,6 +33,11 @@ Vue.use(ModelInput)
 Vue.use(Search)
 Vue.use(GroupList)
 Vue.use(Button)
+Vue.use(Create)
+Vue.use(Dialog)
+
+Vue.use(LoadingPlugin)
+Vue.use(ToastPlugin)
 
 Vue.config.productionTip = false
 
