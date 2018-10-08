f74c4840342a3c20bf67fdea1435ea6dd1edb48b
bliky <blikylee@126.com>
2018-10-08 22:26:02 +0800

    基于customer软件架构开发tcworker

--- src/apps/tcworker/router/index.js
+++ src/apps/tcworker/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from 'tcworker/modules/Index'
+import routes from './routes'
+import routesModelInput from 'components/ModelInput/router/routes'
+import { beforeEach, afterEach } from '@/hooks/router'
+import prefixRoutes from 'utils/prefixRoutes'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index
-    }
-  ]
+const router = new Router({
+  mode: 'history',
+  routes: routes.concat(prefixRoutes('/tcworker', routesModelInput))
 })
+
+router.beforeEach(beforeEach)
+router.afterEach(afterEach)
+
+export default router
