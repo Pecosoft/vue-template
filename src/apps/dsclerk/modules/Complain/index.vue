0e3df732b3b97b5101a804a3eee7cb38ce0ed53b
bliky <blikylee@126.com>
2018-10-11 08:16:12 +0800

    [add]dsclerk添加Tab

--- src/apps/dsclerk/modules/Complain/index.vue
+++ src/apps/dsclerk/modules/Complain/index.vue
@@ -1,28 +1,107 @@
 <template lang="pug">
   page
-    complain-list(:data='list' style='margin-bottom: 60px')
-    .peco-fixed-bottom-area
-      ul.peco-h-flex-btns
-        li.peco-h-flex-btn
-          router-link(:to='{name: "ComplainCreate", query: {cate: 1}}') 产品投诉
-        li.peco-h-flex-btn
-          router-link(:to='{name: "ComplainCreate", query: {cate: 2}}') 服务投诉
+    tab(:tabs='tabs' v-model='tabIndex')
+    keep-alive
+      template(v-if='tabIndex == 0')
+        talk-card(v-for='item in list0' :key='item.id' :header='item.user' :btns='btns' @click.native='handleOnClickTalkCard(item)' @clickbtn='handleOnClickBtn')
+          template(v-if='item.cate==1')
+            p 产品型号：
+              span.gray {{ item.product_model }}
+            p 机台编号：
+              span.gray {{ item.machine_sn }}
+          p 投诉描述：
+            span.gray {{ item.description }}
+          p(style='margin-top: 15px') 联系电话：
+            span.link {{ item.contact }}
+      template(v-else-if='tabIndex == 1')
+        talk-card(v-for='item in list1' :key='item.id' :header='item.user' :btns='btns' @click.native='handleOnClickTalkCard(item)' @clickbtn='handleOnClickBtn')
+          template(v-if='item.cate==1')
+            p 产品型号：
+              span.gray {{ item.product_model }}
+            p 机台编号：
+              span.gray {{ item.machine_sn }}
+          p 投诉描述：
+            span.gray {{ item.description }}
+          p(style='margin-top: 15px') 联系电话：
+            span.link {{ item.contact }}
+      template(v-else)
+        talk-card(v-for='item in list2' :key='item.id' :header='item.user' :btns='btns' @click.native='handleOnClickTalkCard(item)' @clickbtn='handleOnClickBtn')
+          template(v-if='item.cate==1')
+            p 产品型号：
+              span.gray {{ item.product_model }}
+            p 机台编号：
+              span.gray {{ item.machine_sn }}
+          p 投诉描述：
+            span.gray {{ item.description }}
+          p(style='margin-top: 15px') 联系电话：
+            span.link {{ item.contact }}
 </template>
 
 <script>
-import ComplainList from 'components/Business/ComplainList'
+import TalkCard from 'components/Business/TalkCard'
 import { createNamespacedHelpers } from 'vuex'
 const { mapState, mapActions } = createNamespacedHelpers('complain')
 
 export default {
   components: {
-    ComplainList
+    TalkCard
+  },
+  data () {
+    return {
+      tabIndex: 0,
+      tabs: ['未分配', '已分配', '已完成'],
+      btns: [
+        {
+          id: 2,
+          name: '分配/撤回'
+        },
+        {
+          id: 1,
+          name: '联系客户'
+        }
+      ]
+    }
   },
   computed: {
-    ...mapState(['list'])
+    ...mapState(['list']),
+    list0 () {
+      let list = this.list
+      return list.filter(item => {
+        if (item.status === 0) {
+          return true
+        }
+        return false
+      })
+    },
+    list1 () {
+      let list = this.list
+      return list.filter(item => {
+        let status = item.status
+        if (status === 1) {
+          return true
+        }
+        return false
+      })
+    },
+    list2 () {
+      let list = this.list
+      return list.filter(item => {
+        let status = item.status
+        if (status === 2) {
+          return true
+        }
+        return false
+      })
+    }
   },
   methods: {
-    ...mapActions(['fetch'])
+    ...mapActions(['fetch']),
+    handleOnClickTalkCard (item) {
+      this.$router.push({ name: 'ComplainDetail', params: { id: item.id } })
+    },
+    handleOnClickBtn (btn) {
+      console.log(btn)
+    }
   },
   mounted () {
     this.$peco.loading.show()
@@ -34,5 +113,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import 'dsclerk/style.scss';
+@import 'dsclerk/style.scss'
 </style>
