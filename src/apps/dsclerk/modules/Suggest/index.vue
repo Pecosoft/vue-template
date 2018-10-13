6406f4fdfe27e4d6596f5368e27416d9598cf899
bliky <blikylee@126.com>
2018-10-13 23:51:05 +0800

    dsclerk增加维修派单,投诉分配,回复建议

--- src/apps/dsclerk/modules/Suggest/index.vue
+++ src/apps/dsclerk/modules/Suggest/index.vue
@@ -1,31 +1,18 @@
 <template lang="pug">
   page
-    talk-card(v-for='item in list' :key='item.id' :header='item.user' @click.native='handleOnClickTalkCard(item)')
-      p 产品：
-        span.gray {{ item.product_model }}
-      p 建议：
-        span.gray {{ item.description }}
-      p(style='margin-top: 15px') 联系电话：
-        span.link {{ item.contact }}
+    suggest-list(:data='list')
 </template>
 
 <script>
-import TalkCard from 'components/Business/TalkCard'
 import { createNamespacedHelpers } from 'vuex'
 const { mapState, mapActions } = createNamespacedHelpers('suggest')
 
 export default {
-  components: {
-    TalkCard
-  },
   computed: {
     ...mapState(['list'])
   },
   methods: {
-    ...mapActions(['fetch']),
-    handleOnClickTalkCard (item) {
-      this.$router.push({ name: 'ComplainDetail', params: { id: item.id } })
-    }
+    ...mapActions(['fetch'])
   },
   mounted () {
     this.$peco.loading.show()
