03ddef0af3174ec48b8fbd0d668cf74cddc5f36d
bliky <blikylee@126.com>
2018-10-21 12:24:42 +0800

    gpleader增加用户授权信息

--- src/apps/gpleader/store/index.js
+++ src/apps/gpleader/store/index.js
@@ -4,6 +4,7 @@ import ProductPicker from 'components/ModelInput/store/modules/ProductPicker'
 import repair from './modules/repair'
 import complain from './modules/complain'
 import dispatch from './modules/dispatch'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
@@ -12,7 +13,8 @@ const store = new Vuex.Store({
     ProductPicker,
     repair,
     complain,
-    dispatch
+    dispatch,
+    user
   }
 })
 
