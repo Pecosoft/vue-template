f087a27981561bc135376eaf49f3ea507b292e5a
bliky <blikylee@126.com>
2018-10-21 10:42:41 +0800

    auth.js增加name和avatar

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,11 +2,25 @@ import Cookie from 'js-cookie'
 
 const auth = {
   set (query) {
-    let { uid, appid, ticket } = query
+    let { uid, appid, ticket, name, avatar } = query
     if (uid && appid && ticket) {
       Cookie.set('peco_user_id', uid)
       Cookie.set('peco_app_id', appid)
       Cookie.set('peco_ticket', ticket)
+    }
+    if (name && avatar) {
+      Cookie.set('peco_user_name', name)
+      Cookie.set('peco_user_avatar', avatar)
+    }
+  },
+  user () {
+    let id = Cookie.get('peco_user_id') || '-'
+    let name = Cookie.get('peco_user_name') || '-'
+    let avatar = Cookie.get('peco_user_avatar') || '-'
+    return {
+      id,
+      name,
+      avatar
     }
   },
   get () {
