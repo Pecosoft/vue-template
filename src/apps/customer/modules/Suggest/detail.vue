8ce146cf4d9b205456315393aa5d945b8666370a
bliky <blikylee@126.com>
2018-11-11 15:16:49 +0800

    投诉和建议详情业数据接入

--- src/apps/customer/modules/Suggest/detail.vue
+++ src/apps/customer/modules/Suggest/detail.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
 page
-  talk-card(:data='suggestDetail' :btns='btns' @clickbtn='handleOnClickBtn')
+  talk-card(v-if='suggestDetail.description' :data='suggestDetail')
     p 产品：
       span.gray {{ suggestDetail.product_model }}
     p 建议：
       span.gray {{ suggestDetail.description }}
     p(style='margin-top: 15px') 联系电话：
       span.link {{ suggestDetail.contact }}
-  div(style='padding: 20px 15px 10px; font-size: 14px;') 回复：
-  talk-card(:data='suggestDetail.reply' :btns='btns' @clickbtn='handleOnClickBtn')
-    p 回复：
-      span.gray {{ suggestDetail.reply.content }}
+  template(v-if='suggestDetail.status === 1 && suggestDetail.reply')
+    div(style='padding: 20px 15px 10px; font-size: 14px;') 回复：
+    talk-card(:data='suggestDetail.reply')
+      p 回复：
+        span.gray {{ suggestDetail.reply.content }}
 </template>
 
 <script>
@@ -27,24 +28,6 @@ export default {
     return {
       id: this.$route.params.id,
       suggestDetail: {
-        user: {
-          name: '',
-          intro: '',
-          avatar: '/static/logo.png',
-          datetime: new Date().getTime()
-        },
-        product_model: '',
-        description: '',
-        contact: '',
-        reply: {
-          user: {
-            name: '',
-            intro: '',
-            avatar: '/static/logo.png',
-            datetime: new Date().getTime()
-          },
-          content: ''
-        }
       },
       btns: [
       ]
