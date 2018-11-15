e490de00e9f08a039c2aed0a780c52ef0cd0ed85
bliky <blikylee@126.com>
2018-11-15 23:54:05 +0800

    投诉分配增加管理员

--- src/apps/dsclerk/store/modules/dispatch.js
+++ src/apps/dsclerk/store/modules/dispatch.js
@@ -1,15 +1,20 @@
 /* eslint-disable */
-import { gpleader } from '@/services'
+import { gpleader, manager } from '@/services'
 
 export default {
   namespaced: true,
   state: {
     list: [],
-    mappick: {}
+    list2: [],
+    mappick: {},
+    mappick2: {}
   },
   mutations: {
     ['DISPATCH'] (state, { id, picked }) {
       state.mappick[id] = picked
+    },
+    ['DISPATCH2'] (state, { id, picked }) {
+      state.mappick2[id] = picked
     }
   },
   actions: {
@@ -18,6 +23,12 @@ export default {
       let res = await gpleader.fetch()
       state.list = res
       return res
+    },
+    async fetch2 ({ state }) {
+      if (state.list2.length) return state.list2
+      let res = await manager.fetch()
+      state.list2 = state.list.concat(res)
+      return state.list2
     }
   }
 }
