323c0f7206ae10d0ab62f8a9c2e9ed061d070099
bliky <blikylee@126.com>
2018-11-26 00:06:57 +0800

    创建报修增加微信SDK选择图片，启动录音

--- src/apps/customer/store/modules/opendata.js
+++ src/apps/customer/store/modules/opendata.js
@@ -1,12 +1,15 @@
 /* eslint-disable */
 import { provinceCityArea } from '@/services/opendata'
+import wxjssdk from '@/services/wxjssdk'
 
 export default {
   namespaced: true,
   state: {
-    province_city_areas: []
+    province_city_areas: [],
+    wxjssdkConfig: null
   },
   mutations: {
+
   },
   actions: {
     async provinceCityArea ({ state }) {
@@ -14,6 +17,13 @@ export default {
       if (province_city_areas && province_city_areas.length) return province_city_areas
       let res = await provinceCityArea()
       state.province_city_areas = res
+      return res
+    },
+    async getWxjssdkConfig ({ state }) {
+      let wxjssdkConfig = state.wxjssdkConfig
+      if (wxjssdkConfig) return wxjssdkConfig
+      let res = await wxjssdk()
+      state.wxjssdkConfig = res
       return res
     }
   }
