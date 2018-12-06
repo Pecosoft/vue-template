17f00b2f825e36b38c7fee6015a6541c1f843130
bliky <blikylee@126.com>
2018-12-07 01:01:46 +0800

    维修师傅到场定位

--- src/main-tcworker.js
+++ src/main-tcworker.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// import VConsole from 'vconsole'
 import App from './App'
 import router from 'tcworker/router'
 import store from 'tcworker/store'
@@ -26,9 +27,10 @@ import Tab from 'components/Tab'
 import LoadingPlugin from 'plugins/loading'
 import ToastPlugin from 'plugins/toast'
 
-import { AlertPlugin } from 'vux'
-Vue.use(AlertPlugin)
+import { AlertPlugin, TransferDom } from 'vux'
 
+Vue.use(AlertPlugin)
+Vue.directive('transfer-dom', TransferDom)
 Vue.use(Page)
 Vue.use(ProfileHeader)
 Vue.use(Grid)
@@ -54,6 +56,9 @@ Vue.config.productionTip = false
 if (process.env.NODE_ENV !== 'production') {
   // require('./services/mock').default.bootstrap()
 }
+
+/* eslint-disable no-new */
+// new VConsole()
 
 /* eslint-disable no-new */
 new Vue({
