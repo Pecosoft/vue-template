2512c12226a9fe2e7ccb093379ff37bcfe5d355b
bliky <blikylee@126.com>
2019-03-24 15:06:50 +0800

    结案增加收费金额字段

--- src/apps/tcworker/modules/Repair/complete.vue
+++ src/apps/tcworker/modules/Repair/complete.vue
@@ -5,9 +5,10 @@ page
     datetime(title='出机日期' v-model='date' :end-date='today' placeholder='请选择出机日期')
     popup-radio(title='维保说明' :options="rangeOptions" v-model='range')
     popup-radio(title='收费情况' :options="chargedOptions" v-model='charged')
+    x-input(title='收费金额' placeholder='请填写收费金额' v-model='charges')
     x-input(title='更换配件' placeholder='请填写更换的配件' v-model='fittings')
     x-textarea(title='维修内容' placeholder='请对维修内容进行描述' v-model='content')
-    x-textarea(title='备注' placeholder='填写其他备注' v-model='comment')
+    //- x-textarea(title='备注' placeholder='填写其他备注' v-model='comment')
   div(style='padding: 15px')
     x-button(plain @click.native='handleOnClickCancel') 取消
     x-button(type='primary' @click.native='handleOnClickSave') 提交
@@ -21,7 +22,7 @@ import { repair } from '@/services'
 export default {
   data () {
     let dt = new Date()
-    let today = dt.getFullYear() + '-' + dt.getMonth() + '-' + dt.getDate()
+    let today = dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate()
     return {
       rangeOptions: [
         {
@@ -49,6 +50,7 @@ export default {
       date: today,
       range: 0,
       charged: 0,
+      charges: 0,
       fittings: '',
       content: '',
       comment: ''
@@ -66,14 +68,20 @@ export default {
       this.$router.go(-1)
     },
     handleOnClickSave () {
-      let { id, num, date, range, charged, fittings, content, comment } = this
+      let { id, num, date, range, charged, charges, fittings, content, comment } = this
       if (!id || !num || !content) {
         return this.$vux.alert.show({
           title: '出机编号和维修内容为必填',
           content: '请填写完整结案信息'
         })
       }
-      let formData = { num, date, range, charged, fittings, content, comment }
+      if (isNaN(parseInt(charges))) {
+        return this.$vux.alert.show({
+          title: '收费金额必须为数字',
+          content: '请正确填写收费金额'
+        })
+      }
+      let formData = { num, date, range, charged, charges, fittings, content, comment }
       this.$peco.loading.show()
       repair.update(id, {
         ...formData,
